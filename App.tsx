2732d1852658c3a56ac483ea27523ac5c7e9293d
SaberFate01 <39643464+SaberFate01@users.noreply.github.com>
2023-08-23 12:23:51 +1000

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,132 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Apex VI native template">
+            For testing purpose
+          </Section>
+          <Section title="Apex VI native template">
+            For testing purpose
+          </Section>
+          <Section title = "Dunno what to put here">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+              molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+              numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+              optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+              obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+              nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+              tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+              quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+              sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+              recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
+              minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
+              quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
+              fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
+              consequuntur! Commodi minima excepturi repudiandae velit hic maxime
+              doloremque. Quaerat provident commodi consectetur veniam similique ad 
+              earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo 
+              fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore 
+              suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium
+              modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam 
+              totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam 
+              quasi aliquam eligendi, placeat qui corporis!
+          </Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
