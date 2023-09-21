fdf4c5f5665f57bd31890603c43c06b88059e2d0
Prakash <p.palanivelurajmohan@uqconnect.edu.au>
2023-09-22 09:41:48 +1000

    fix chat

--- screens/ChatPageGroup.js
+++ screens/ChatPageGroup.js
@@ -4,7 +4,7 @@ import { WebView } from 'react-native-webview';
 const ChatPageGroup = () => {
   return (
     <WebView 
-      source={{ uri: "https://spring-dot-acoustic-cirrus-396009.ts.r.appspot.com/" }}
+      source={{ uri: "https://acoustic-cirrus-396009.ts.r.appspot.com/" }}
       style={{ flex: 1 }}
     />
   );
